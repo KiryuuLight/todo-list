5ab2e38c4cd95ee609bfc8e25335466277c06b34
Luis O <70501251+KiryuuLight@users.noreply.github.com>
2023-03-20 18:00:32 -0500

    Rename (addTask, removeTask) and create new functions (generateIdCount , assignTaskId)

--- src/project.js
+++ src/project.js
@@ -1,41 +1,50 @@
+import { numberGenerator } from './dinamicNumber';
+
 /* Project factory */
 
 export const createProject = (title, description) => {
-  const todos = [];
+    let taskList = [];
 
-  const getTitle = () => title;
+    const getTitle = () => title;
 
-  const getDescription = () => description;
+    const setTitle = (newTitle) => (title = newTitle);
 
-  const getTodos = () => todos;
+    const getDescription = () => description;
 
-  return {
-    getTitle,
-    getDescription,
-    getTodos,
-  };
+    const setDescription = (newDescription) => (description = newDescription);
+
+    const getTaskList = () => taskList;
+
+    const setTaskList = (newList) => (taskList = newList);
+
+    return {
+        getTitle,
+        setTitle,
+        getDescription,
+        setDescription,
+        getTaskList,
+        setTaskList,
+    };
 };
 
-export const addTodoToProject = (project, todoObj) => {
-  project.getTodos().push(todoObj);
-  return console.log('Todo Added to the Project');
+export const addTask = (project, task) => {
+    project.getTaskList().push(task);
+    return console.log('Task Added to the Project');
 };
 
-/* Todo factory */
+export const removeTask = (project, id) => {
+    const arr = project.getTaskList().filter((value) => value.getId() !== id);
+    project.setTaskList(arr);
+    if (arr !== undefined)
+        return `Task with id : ${id} was removed from the Task List`;
+    return 'Id not found';
+};
 
-export const createTodo = (title, description, dueDate, priority) => {
-  const getTitle = () => title;
+export const generateIdCount = () => {
+    const generateNumber = numberGenerator();
+    return generateNumber;
+};
 
-  const getDescription = () => description;
-
-  const getDueDate = () => dueDate;
-
-  const getPriority = () => priority;
-
-  return {
-    getTitle,
-    getDescription,
-    getDueDate,
-    getPriority,
-  };
+export const assignTaskId = (idCount, task) => {
+    task.setId(idCount);
 };
