863093c4be2bec628b4cb926b6664cb1539badf7
Luis O <70501251+KiryuuLight@users.noreply.github.com>
2023-04-09 15:58:53 -0500

    Add file to manage img paths

---
+++ src/utils/loadImages.js
@@ -0,0 +1,13 @@
+import arrowSvg from '../img/arrow.svg';
+import editSvg from '../img/edit.svg';
+import removeSvg from '../img/remove.svg';
+import plusSvg from '../img/plus.svg';
+import squareSvg from '../img/square.svg';
+
+export default {
+    arrowSvg,
+    editSvg,
+    removeSvg,
+    plusSvg,
+    squareSvg,
+};
